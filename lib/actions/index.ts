b44a9a95aaba6ff8695dae0b75c190aa0bb36f38
Adrian Rusan <rusan.adrian.ionut@gmail.com>
2023-10-10 15:58:48 +0300

    Setup and Connect to Database

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,15 +1,62 @@
 'use server';
 
-import { scrapeEmagProduct } from '../scrapper';
+import { revalidatePath } from 'next/cache';
+import Product from '../models/product.model';
+import { connectToDB } from '../mongoose';
+import { scrapeEmagProduct } from '../scraper';
+import { getAveragePrice, getHighestPrice, getLowestPrice } from '../utils';
 
 export async function scrapeAndScoreProduct(productUrl: string) {
   if (!productUrl) return;
 
   try {
-    const scrappedProduct = await scrapeEmagProduct(productUrl);
+    connectToDB();
 
-    if (!scrappedProduct) return;
+    const scrapedProduct = await scrapeEmagProduct(productUrl);
+
+    if (!scrapedProduct) return;
+
+    let product = scrapedProduct;
+
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+    if (existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      };
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+
+    revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`);
+  }
+}
+
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
+
+    const product = await Product.findOne({ _id: productId });
+
+    if (!product) return;
+
+    return product;
+  } catch (error) {
+    console.log(error);
   }
 }
