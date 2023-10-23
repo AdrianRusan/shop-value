302bd406d714d2d1b831d32f64c27571e0aafb23
Adrian Rusan <rusan.adrian.ionut@gmail.com>
2023-10-23 15:57:07 +0300

    small fix

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,20 +1,10 @@
 'use client'
 
-import { useState, useEffect } from 'react'
-import { useTheme } from '@/app/theme-provider'
-import { Switch } from '@headlessui/react'
+import { useTheme } from '@/app/theme-provider';
+import { Switch } from '@headlessui/react';
 
 const ThemeSwitch = () => {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) {
-    return null
-  }
+  const { theme, setTheme } = useTheme();
 
   return (
     <Switch
@@ -24,10 +14,10 @@ const ThemeSwitch = () => {
     >
       <span className="sr-only">Switch theme</span>
       <span
-        className='translate-x-1 dark:translate-x-6 inline-block h-4 w-4 transform rounded-full bg-white dark:bg-black transition'
+        className="translate-x-1 dark:translate-x-6 inline-block h-4 w-4 transform rounded-full bg-white dark:bg-black transition"
       />
     </Switch>
-  )
-}
+  );
+};
 
-export default ThemeSwitch
+export default ThemeSwitch;
