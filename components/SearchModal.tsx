c9daec797a76a4471b7fbea0fd067e5f845ad6d3
Adrian Rusan <rusan.adrian.ionut@gmail.com>
2023-10-23 13:02:31 +0300

    small fix

--- components/SearchModal.tsx
+++ components/SearchModal.tsx
@@ -5,7 +5,7 @@ import { Dialog, Transition } from '@headlessui/react';
 import Image from 'next/image';
 import { Product } from '@/types';
 import { getProductByTitle } from '@/lib/actions';
-import dynamic from 'next/dynamic';
+import ThemedIcon from './ThemedIcon';
 
 
 const SearchModal = () => {
@@ -41,8 +41,6 @@ const SearchModal = () => {
     e.preventDefault();
     closeModal();
   };
-
-  const ThemedIcon = dynamic(() => import('./ThemedIcon'));
 
   return (
     <>
